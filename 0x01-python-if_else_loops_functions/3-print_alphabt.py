d2d14398ec8cf8ee17bca5cf6f46508f15d6420c
fawazabdganiyu <fawazabdganiyu@gmail>
2023-08-09 00:28:07 +0100

    Print in lowercase except q and e

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''prints the ASCII alphabet, in lowercase, not followed by a new line, except q and e'''
+character = 97
+while character >= 97 and character <= 122:
+    if chr(character) == 'q' or chr(character) == 'e':
+        character += 1
+        continue
+    print("{}".format(chr(character)), sep='', end='')
+    character += 1
