ab611248dd4b78b03242342ef5ab73f01224d4f9
fawazabdganiyu <fawazabdganiyu@gmail>
2023-08-09 18:18:52 +0100

    Amend code

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -11,40 +11,37 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new_node, *current;
 
-	if (*head == NULL)
-		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->n = number;
+	/* Consider when the number is smaller than the first node data */
+	if (*head == NULL)
+	{
+		*head = new_node;
+		new_node->next = NULL;
+		return (new_node);
+	}
+	/* Consider other posibilities */
 	current = *head;
-	if (number < current->n)
+	/* Consider when the number is <=  the first node data */
+	if (number <= current->n)
 	{
 		new_node->next = *head;
 		*head = new_node;
-		return (new_node);
 	}
 	while (current)
 	{
-		if (number > current->n && current->next == NULL)
-		{
-			new_node->next = NULL;
-			current->next = new_node;
-			return (new_node);
-		}
-		if (current->n < number && number < current->next->n)
-		{
-			new_node->next = current->next;
-			current->next = new_node;
-			return (new_node);
-		}
-		if (number > current->n && current->next == NULL)
-		{
-			new_node->next = current->next;
-			current->next = new_node;
-			return (new_node);
-		}
+		/* Consider when even the last data is < number */
+		if (number >= current->n && current->next == NULL)
+			break;
+		/* Insert the node at the expected position */
+		if (current->n <= number && number <= current->next->n)
+			break;
 		current = current->next;
 	}
-	return (NULL);
+	new_node->next = current->next;
+	current->next = new_node;
+	return (new_node);
 }
