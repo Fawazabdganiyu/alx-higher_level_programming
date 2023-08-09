28e8d64cd24b0ab39fdd0ca238cbb773b1b3bf90
fawazabdganiyu <fawazabdganiyu@gmail>
2023-08-09 01:49:28 +0100

    Use appropriate number of print()

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """prints numbers from 0 to 99"""
 for num in range(100):
-    if num < 10:
-        print("{:d}{:d}".format(0, num),  end=', ')
+    if num < 99:
+        print("{:02d}".format(num),  end=', ')
         continue
-    elif num < 99:
-        print("{:d}".format(num), end=', ')
     else:
         print("{:d}".format(num))
