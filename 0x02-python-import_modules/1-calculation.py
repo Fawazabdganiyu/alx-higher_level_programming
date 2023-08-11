b1234fbfe3c42a62810e06a957f9cc0755d8541f
fawazabdganiyu <fawazabdganiyu@gmail>
2023-08-11 15:17:33 +0100

    Call the functions separately

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,11 @@ if __name__ == "__main__":
     a = 10
     b = 5
     from calculator_1 import add, sub, mul, div
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    result = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, result))
+    result = sub(a, b)
+    print("{:d} - {:d} = {:d}".format(a, b, result))
+    result = mul(a, b)
+    print("{:d} * {:d} = {:d}".format(a, b, result))
+    result = div(a, b)
     print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
