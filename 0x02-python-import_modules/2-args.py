001f8a885f9b48ee9afe6fc24dbb3ded56eb5a5c
fawazabdganiyu <fawazabdganiyu@gmail>
2023-08-11 12:02:38 +0100

    Modify the import variant

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """prints argument lists and their number"""
-    import sys
-    args  = len(sys.argv) - 1
+    from sys import argv
+    args  = len(argv) - 1
     if args >= 1:
         print("{:d} {:s}:".format(args, "argument" if args == 1 else "arguments"))
         index = 1
         while index <= args:
-            print("{:d}: {:s}".format(index , sys.argv[index]))
+            print("{:d}: {:s}".format(index , argv[index]))
             index += 1
     else:
         print("0 arguments.")
