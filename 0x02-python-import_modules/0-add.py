11f74dd54e0f89e52ac05ec7a46acd03a61258ea
fawazabdganiyu <fawazabdganiyu@gmail>
2023-08-11 10:25:38 +0100

    Remove "add_0" from the comment

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """that imports the function def add(a, b): from the file add_0.py
-    and prints the result of the addition 1 + 2 = 3
+    """imports  function add from a file and prints
+    the result of the addition 1 + 2 = 3
     """
     from add_0 import add
     a = 1
     b = 2
-    result = add(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, result))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
