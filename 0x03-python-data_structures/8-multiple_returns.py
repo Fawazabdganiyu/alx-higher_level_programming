6f72ef0e599ec57a981b07f7b38991defd6bb5ec
fawazabdganiyu <fawazabdganiyu@gmail>
2023-08-15 00:18:21 +0100

    Implement when string is empty

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@ def multiple_returns(sentence):
     """returns a tuple with the length of a string and its first character.
     """
     length = len(sentence)
-    str_0 = sentence[0]
+    str_0 = sentence[0] if length != 0 else None
     tuple_pack = length, str_0
     return tuple_pack
