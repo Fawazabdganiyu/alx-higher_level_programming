d74774da21fedf3184de92d0899252a836b0b33a
fawazabdganiyu <fawazabdganiyu@gmail>
2023-08-15 00:11:28 +0100

    Implement tuple packing

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """returns a tuple with the length of a string and its first character.
+    """
+    length = len(sentence)
+    str_0 = sentence[0]
+
+    return length, str_0
