3e9ff692d4029f319b22767b277267c4556e48da
fawazabdganiyu <fawazabdganiyu@gmail>
2023-08-17 01:42:14 +0100

    Return a new list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,6 +3,6 @@ def uniq_add(my_list=[]):
     """adds all unique integers in a list (only once for each integer).
     """
     from functools import reduce
-    my_list = set(my_list)
+    my_list = set(my_list[:])
     adds = reduce(lambda a, b: a + b, my_list)
     return adds
