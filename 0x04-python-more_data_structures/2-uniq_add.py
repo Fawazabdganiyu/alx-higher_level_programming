472f96d213d902713e40d15dca6fc99df09329f5
fawazabdganiyu <fawazabdganiyu@gmail>
2023-08-17 01:47:40 +0100

    Remove reduce module

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,7 +2,8 @@
 def uniq_add(my_list=[]):
     """adds all unique integers in a list (only once for each integer).
     """
-    from functools import reduce
-    my_list = set(my_list[:])
-    adds = reduce(lambda a, b: a + b, my_list)
+    my_list = set(my_list)
+    adds = 0
+    for x in my_list:
+        adds += x
     return adds
