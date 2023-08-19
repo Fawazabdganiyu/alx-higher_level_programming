fd6742ddb42a3525ed2d59e4be6eccdc2573ade0
fawazabdganiyu <fawazabdganiyu@gmail>
2023-08-20 00:07:55 +0100

    Consider a NULL argument

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,7 @@
 def print_reversed_list_integer(my_list=[]):
     """prints all integers of a list, in reverse order.
     """
-    my_list.reverse()
-    for elem in my_list:
-        print("{:d}".format(elem))
+    if my_list is not None:
+        my_list.reverse()
+        for elem in my_list:
+            print("{:d}".format(elem))
