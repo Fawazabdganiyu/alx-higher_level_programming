d849cc31a5678f2759476375178c7e226b150f23
fawazabdganiyu <fawazabdganiyu@gmail>
2023-08-28 20:05:08 +0100

    Remove the clean up statement

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,8 +11,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end='')
         print()
+        return i + 1
     except IndexError:
         print()
-        i -= 1
-    finally:
-        return i + 1
+        return i
