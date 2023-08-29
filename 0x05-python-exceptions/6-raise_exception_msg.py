2ccc4a423ab7706dcc3aea16a75447efce3853ee
fawazabdganiyu <fawazabdganiyu@gmail>
2023-08-29 05:18:11 +0100

    Raise Name exception

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """raises a name exception with a message.
+    """
+    raise NameError(message)
+
+
+if __name__ == "__main__":
+    raise_exception_msg(message="")
