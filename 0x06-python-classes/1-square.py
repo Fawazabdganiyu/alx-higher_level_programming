6199c3b2a76ccb7099516c2f539d0ef8e9521e4e
fawazabdganiyu <fawazabdganiyu@gmail>
2023-08-30 04:59:55 +0100

    Instantiate with private size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """defines a square"""
+    def __init__(self, size):
+        """Initialises the instance attributes
+
+        Args:
+            size: The size of the square
+        """
+        self.__size = size
