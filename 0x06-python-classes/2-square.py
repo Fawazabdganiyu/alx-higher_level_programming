8d41ff6b043d3c37b3a84bac35fbca3bd0c32c84
fawazabdganiyu <fawazabdganiyu@gmail>
2023-08-30 08:28:46 +0100

    Raise exceptions properly

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -14,14 +14,9 @@ class Square:
         Args:
             size (int): The size of the square
         """
-        try:
-            if not isinstance(size, int):
-                raise TypeError
-            if size < 0:
-                raise ValueError
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
