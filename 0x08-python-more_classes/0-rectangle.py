ddbafdc05c77fc7fe21bbec6fbe2795bf6f62baa
fawazabdganiyu <fawazabdganiyu@gmail>
2023-09-04 15:45:47 +0100

    Implement pycodestyle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -5,6 +5,8 @@ Description: A module that provide ``Rectangle`` class
 Author: Fawaz Abdganiyu <fawazabdganiyu@gmail.com>
 
 """
+
+
 class Rectangle:
     """An empty class that defines a rectangle"""
     pass
