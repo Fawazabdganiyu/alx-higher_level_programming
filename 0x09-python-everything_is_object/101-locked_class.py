a87e64dd75eba430b1b3b74250bcd602d736bbbd
fawazabdganiyu <fawazabdganiyu@gmail>
2023-09-06 21:58:45 +0100

    Make it pythonic

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -15,9 +15,4 @@ class LockedClass:
     instance attribute is called first_name
 
     """
-    def __setattr__(self, name, value):
-        if name == "first_name":
-            super().__setattr__(name, value)
-        else:
-            raise AttributeError("'LockedClass' " + "object has no attribute "
-                                 + repr(name))
+    __slots__ = ["first_name"]
