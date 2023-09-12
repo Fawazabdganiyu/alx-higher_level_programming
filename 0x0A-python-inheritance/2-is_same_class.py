17cf739d944f580d2cdc61c77ebb1b417e58b1d6
fawazabdganiyu <fawazabdganiyu@gmail>
2023-09-12 04:24:34 +0100

    Confirm type

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module Name: 2-is_same_class.py
+Description: This module provide ``is_same_class`` function
+Author: Fawaz Abdganiyu <fawazabdganiyu@gmail.com>
+
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    confirms if isinstance of a class
+
+    Args:
+        obj (obj): The object to confirm its class
+        a_class (class): The class to confirm if an object is exactly its
+        instance.
+
+    Return: True if the objext is exactly an instance of the specified class,
+    otherwise False.
+
+    """
+    return type(obj) == a_class
