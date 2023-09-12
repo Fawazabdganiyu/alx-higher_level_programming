e3ca4978e187c6107fb46980c80e95f1c48d59f8
fawazabdganiyu <fawazabdganiyu@gmail>
2023-09-12 04:54:35 +0100

    Create empty Geometry module

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module Name: 5-base_geometry.py
+Description: This module provide ``inherits_from`` function
+Author: Fawaz Abdganiyu <fawazabdganiyu@gmail.com>
+
+"""
+
+
+class BaseGeometry:
+    """
+    A simple ``BaseGeometry`` class
+    """
+    pass
