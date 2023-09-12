9142a084731af9cd10a58f144665616f22eb229f
fawazabdganiyu <fawazabdganiyu@gmail>
2023-09-12 05:03:13 +0100

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module Name: 6-base_geometry.py
+Description: This module provide ``BaseGeometry`` class
+Author: Fawaz Abdganiyu <fawazabdganiyu@gmail.com>
+
+"""
+
+
+class BaseGeometry:
+    """
+    A simple ``BaseGeometry`` class
+    """
+    def area(self):
+        """
+        Not implemented but raises an exception.
+        """
+        raise Exception("area() is not implemented")
