492b9599ec65efad9ddb315154e8392f47b50270
fawazabdganiyu <fawazabdganiyu@gmail>
2023-09-13 17:25:57 +0100

    Improve code

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -5,65 +5,7 @@ Description: This module provide ``BaseGeometry`` class
 Author: Fawaz Abdganiyu <fawazabdganiyu@gmail.com>
 
 """
-
-
-class BaseGeometry:
-    """
-    A simple ``BaseGeometry`` class
-    """
-    def area(self):
-        """
-        Not implemented but raises an exception.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        validates an integer
-
-        Args:
-            name (str): The name of the argument
-            value (int): The argument to be validated.
-
-        Raises:
-            TypeError: If value is not an integer
-            ValueError: If value is less than or greater than zero
-        """
-        if type(value) != int:
-            raise TypeError(f"{value} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{value} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    """A representation of Rectangle class
-    """
-    def __init__(self, width, height):
-        """instantiation
-
-        Args:
-            width (int): The width of the geometry
-            height (int): The height of the geometry
-
-        """
-        super().integer_validator("width", width)
-        self.__width = width
-        super().integer_validator("height", height)
-        self.__height = height
-
-    def __str__(self):
-        """Returns ``str`` representation of the rectangle
-        """
-        rect_class = str(self.__class__.__name__)
-        rect_width = str(self.__width)
-        rect_height = str(self.__height)
-        return f"[{rect_class}] {rect_width}/{rect_height}"
-
-    def area(self):
-        """
-        Returns the area of the rectangle
-        """
-        return self.__width * self.__height
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -5,34 +5,7 @@ Description: This module provide ``BaseGeometry`` class
 Author: Fawaz Abdganiyu <fawazabdganiyu@gmail.com>
 
 """
-
-
-class BaseGeometry:
-    """
-    A simple ``BaseGeometry`` class
-    """
-    def area(self):
-        """
-        Not implemented but raises an exception.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        validates an integer
-
-        Args:
-            name (str): The name of the argument
-            value (int): The argument to be validated.
-
-        Raises:
-            TypeError: If value is not an integer
-            ValueError: If value is less than or greater than zero
-        """
-        if type(value) != int:
-            raise TypeError(f"{value} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{value} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
