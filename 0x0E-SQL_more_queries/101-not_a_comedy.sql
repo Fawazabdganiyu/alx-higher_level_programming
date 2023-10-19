526fe4cfe793cbd9d4175c6f4f91b76971650fbd
fawazabdganiyu <fawazabdganiyu@gmail>
2023-10-19 23:22:16 +0100

    List all shows without "Comedy"

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,1 +1,12 @@
--- Doc
+-- List all shows without the genre "Comedy"
+SELECT title
+  FROM tv_shows
+EXCEPT
+SELECT title
+  FROM tv_shows AS ts
+       INNER JOIN tv_show_genres AS tsg
+       ON ts.id = tsg.show_id
+       INNER JOIN tv_genres AS tg
+       ON tsg.genre_id = tg.id
+       WHERE tg.name = "Comedy"
+ ORDER BY title;
