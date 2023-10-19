947fc596997357ba1d94eb9cd6f8a3c20eee22af
fawazabdganiyu <fawazabdganiyu@gmail>
2023-10-19 12:50:29 +0100

    List shows without a genre

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,1 +1,8 @@
--- Doc
+-- List all shows contained in the database `hbtn_0d_tvshows`
+-- without a genre.
+SELECT ts.title, tsg.genre_id
+  FROM tv_shows AS ts
+       LEFT JOIN tv_show_genres AS tsg
+       ON ts.id = tsg.show_id
+       WHERE tsg.genre_id IS NULL
+ ORDER BY ts.title, tsg.genre_id;
