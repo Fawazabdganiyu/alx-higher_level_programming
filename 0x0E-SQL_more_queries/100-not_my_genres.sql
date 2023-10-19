7abc2cf60d115450b6b86b6955a630eca9e64525
fawazabdganiyu <fawazabdganiyu@gmail>
2023-10-19 23:10:22 +0100

    List all genres except in "Dexter"

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,1 +1,13 @@
--- Doc
+-- List all genres not linked to the show "Dexter"
+SELECT `name`
+  FROM tv_genres
+EXCEPT
+SELECT `name`
+  FROM tv_genres AS tg
+       INNER JOIN tv_show_genres AS tsg
+       ON tg.id = tsg.genre_id
+
+       INNER JOIN tv_shows AS ts
+       ON tsg.show_id = ts.id
+       WHERE ts.title = "Dexter"
+ ORDER BY `name`;
