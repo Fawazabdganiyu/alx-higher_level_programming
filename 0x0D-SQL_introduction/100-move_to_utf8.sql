63848f91fc2fbec66455e1504f7ebdc85dbee3da
fawazabdganiyu <fawazabdganiyu@gmail>
2023-10-19 23:58:06 +0100

    Remove field conversion

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -8,9 +8,3 @@ USE `hbtn_0c_0`
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
-
--- Convert `name` field to UTF8MB4
-ALTER TABLE first_table
-MODIFY name VARCHAR(256)
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
