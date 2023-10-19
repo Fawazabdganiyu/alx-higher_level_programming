c5ee641f2f4e618e94c3b2912333611ca995e695
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-10-18 18:53:35 -0700

    Correct code

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,7 @@
 -- Creates user `user_0d_1`.
 CREATE USER
        IF NOT EXISTS 'user_0d_1'@'localhost'
-       IDENTIFIED WITH 'user_0d_1_pwd';
+       IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges
 GRANT ALL PRIVILEGES
