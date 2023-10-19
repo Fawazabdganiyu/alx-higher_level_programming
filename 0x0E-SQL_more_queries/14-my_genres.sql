abcfd49a2fe48353d6afbf26c097e822e3a4bab4
fawazabdganiyu <fawazabdganiyu@gmail>
2023-10-19 13:30:46 +0100

    List all genres of the show "Dexter"

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,1 +1,10 @@
--- Doc
+-- List all genres of the show `Dexter`.
+SELECT tg.name
+  FROM tv_genres AS tg
+       INNER JOIN tv_show_genres AS tsg
+       ON tg.id = tsg.genre_id
+
+       INNER JOIN tv_shows AS ts
+       ON tsg.show_id = ts.id
+       WHERE ts.title = "Dexter"
+ ORDER BY tg.name;
