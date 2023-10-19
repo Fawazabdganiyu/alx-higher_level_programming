66e132c4d1d6164720594a8068f8f9a801d0e12c
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-10-18 18:49:15 -0700

    Improve code

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,9 @@
 -- Creates user `user_0d_1`.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH 'user_0d_1_pwd';
+CREATE USER
+       IF NOT EXISTS 'user_0d_1'@'localhost'
+       IDENTIFIED WITH 'user_0d_1_pwd';
 
 -- Grant all privileges
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES
+      ON *.*
+      TO 'user_0d_1'@'localhost';
