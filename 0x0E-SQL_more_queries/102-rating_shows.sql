50550bc20cd4af6b1616a6a6a95ac2f1f0c1797b
fawazabdganiyu <fawazabdganiyu@gmail>
2023-10-19 23:45:49 +0100

    List shows base on ratings

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,1 +1,7 @@
--- Doc
+-- List all shows from `hbtn_0d_tvshows_rate` by their rating.
+SELECT ts.title, SUM(tsr.rate) AS rating
+  FROM tv_shows AS ts
+       INNER JOIN tv_show_ratings AS tsr
+       ON ts.id = tsr.show_id
+ GROUP BY ts.title
+ ORDER BY rating DESC;
