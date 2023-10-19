e9887af4d78d90eb6dc39c67fcb6a4446c1366ac
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-10-18 18:42:55 -0700

    Create a server user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Creates user `user_0d_1`.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH 'user_0d_1_pwd';
+
+-- Grant all privileges
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
