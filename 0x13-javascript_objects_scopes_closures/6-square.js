448fb484507f26a366123b019ebb50c8c2dc47c7
fawazabdganiyu <fawazabdganiyu@gmail>
2023-11-14 23:45:33 +0100

    fix: create instance method `charPrint(c)`
    This prints the square using the character in `c`, otherwise it uses
    the charater "X."

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const square = require('./5-square');
+
+class Square extends square {
+  charPrint (c) {
+    if (c !== undefined) {
+      for (let i = 0; i < this.width; ++i) {
+        console.log(c.repeat(this.width));
+      }
+    } else {
+      this.print();
+    }
+  }
+}
+
+module.exports = Square;
