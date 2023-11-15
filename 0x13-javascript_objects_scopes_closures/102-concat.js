add52a4a97ac19df641828ef4d2c88e63cccd32f
fawazabdganiyu <fawazabdganiyu@gmail>
2023-11-15 04:54:01 +0100

    fix: drop error catching
    No instruction was stated on any failures or exceptions handling

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,13 +6,9 @@ const src1 = process.argv[2];
 const src2 = process.argv[3];
 const dest = process.argv[4];
 
-try {
-  const data1 = fs.readFileSync(src1, 'utf-8');
-  const data2 = fs.readFileSync(src2, 'utf-8');
+const data1 = fs.readFileSync(src1, 'utf-8');
+const data2 = fs.readFileSync(src2, 'utf-8');
 
-  const concat = data1 + data2;
+const concat = data1 + data2;
 
-  fs.writeFileSync(dest, concat);
-} catch (e) {
-  // console.error('Usage: ./102-concat.js <FileA_Path> <FileB_Path> <destinationFilePath>');
-}
+fs.writeFileSync(dest, concat);
