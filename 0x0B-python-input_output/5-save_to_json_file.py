1c0dc69c5c2ae27a1c42ddf932ca7b5d9c95a9fe
fawazabdganiyu <fawazabdganiyu@gmail>
2023-11-16 21:27:39 +0100

    Rewrite how the objects is saved to json file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -18,4 +18,4 @@ def save_to_json_file(my_obj, filename):
     import json
 
     with open(filename, mode='w', encoding='utf-8') as f:
-        f.write(json.dumps(my_obj))
+        json.dump(my_obj, f)
