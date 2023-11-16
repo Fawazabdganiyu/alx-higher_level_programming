46dc4092400aa5cf0d04cff03d7cdfbaad48930e
fawazabdganiyu <fawazabdganiyu@gmail>
2023-11-16 21:14:45 +0100

    List all names starting with "N" from a database table

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module Name: 1-filter_states.py
+Description: This module provides a script that lists all `states` with a name
+             starting with "N" from the database `hbtn_0e_0_usa`
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    username, password, database = argv[1:]
+
+    conn = MySQLdb.connect(host='localhost', port=3306, user=username,
+                           passwd=password, db=database, charset='utf8')
+    cur = conn.cursor()
+
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id"
+    cur.execute(query)
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
