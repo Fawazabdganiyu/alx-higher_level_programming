c4b8af5e43c44d231a07008f19cb2a188ba2374d
fawazabdganiyu <fawazabdganiyu@gmail>
2023-11-20 11:52:01 +0100

    fix: Include `City` class definition in the database
    All classes need to be created in the database by skipping over
    the tables which have already been created by `create_all`
    method of `Base.metadata`

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -5,16 +5,17 @@ Description: This is a script that lsits `State` objects and corresponding
              `City` objects, contained in the database `hbtn_0e_101_usa`
 """
 import sys
-from relationship_city import City
-from relationship_state import State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
 
 if __name__ == '__main__':
     user, passwd, database = sys.argv[1:]
     url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(user, passwd, database)
 
     engine = create_engine(url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
