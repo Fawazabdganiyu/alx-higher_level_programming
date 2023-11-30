098e83c8a69593408d81cba122ab3a4c58ad05f5
fawazabdganiyu <fawazabdganiyu@gmail>
2023-12-01 00:52:48 +0100

    fix: make the file executable

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # HTTP request with some parameters
-curl -sd email=test@gmail.com -d subject="I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
