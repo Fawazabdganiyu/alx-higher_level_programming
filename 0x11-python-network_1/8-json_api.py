98ee4b142ac614bc06c3c885c6db24939ecf3fac
fawazabdganiyu <fawazabdganiyu@gmail>
2023-12-02 02:56:54 +0100

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module Name: 8-json_api.py
+Description: Searches for a given letter with POST request to
+             http://0.0.0.0:5000/search_user
+"""
+import sys
+import requests
+
+if __name__ == '__main__':
+    payload = {}
+    payload['q'] = "" if len(sys.argv) == 1 else sys.argv[1]
+
+    res = requests.post('http://0.0.0.0:5000/search_user', data=payload)
+    try:
+        json_body = res.json()
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
+    else:
+        if len(json_body) != 0:
+            print(f"[{json_body.get('id')}] {json_body.get('name')}")
+        else:
+            print("No result")
