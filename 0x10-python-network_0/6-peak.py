6603381036ca1d89f11a446e9f19ebac61f40a72
fawazabdganiyu <fawazabdganiyu@gmail>
2023-12-03 23:34:10 +0100

    fix: use original list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,17 +8,14 @@ Description: Provide a `find_peak` function
 def find_peak(list_of_integers):
     """Find the largest number in a list of unsorted integers
     """
-    int_list = list_of_integers[:]
-    if int_list == []:
-        return None
+    if list_of_integers:
+        peak = 0
+        last = len(list_of_integers) - 1
+        while peak < last:
+            mid = (peak + last) // 2
+            if list_of_integers[mid] < list_of_integers[mid + 1]:
+                peak = mid + 1
+            else:
+                last = mid
 
-    peak = 0
-    last = len(int_list) - 1
-    while peak < last:
-        mid = (peak + last) // 2
-        if int_list[mid] < int_list[mid + 1]:
-            peak = mid + 1
-        else:
-            last = mid
-
-    return int_list[peak]
+        return list_of_integers[peak]
