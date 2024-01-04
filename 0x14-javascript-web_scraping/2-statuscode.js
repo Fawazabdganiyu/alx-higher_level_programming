895b2bb708c62982da796fa2b17336bfad2635da
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-01-04 05:44:08 +0100

    Print a request status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+// Display the status code of a GET request.
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response) {
+  if (error) {
+    console.error('error:', err);
+  }
+  console.log('code:', response.statusCode);
+});
