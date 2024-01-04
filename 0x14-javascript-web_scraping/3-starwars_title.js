9fa6983bce13e16db0c4d6401d804d2b10793654
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-01-04 15:36:13 +0100

    Print Star Wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+// Print a Star Wars movie title where the episode number matches a given integer.
+
+const request = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(JSON.parse(body).title);
+});
