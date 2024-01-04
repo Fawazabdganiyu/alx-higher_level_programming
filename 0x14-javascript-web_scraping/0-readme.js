97ab526d7e882c167032ad20819c6740258ab053
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-01-04 05:42:45 +0100

    Read a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+// Read the content of a file and print it content
+const fs = require('fs');
+const fileName = process.argv[2];
+fs.readFile(fileName, 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+});
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+// Write to a file
+const fs = require('fs');
+const fileName = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(fileName, content, 'utf8', (err) => {
+  if (err) {
+		return console.log(err);
+  }
+});
