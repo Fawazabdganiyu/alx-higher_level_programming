ad52d198ddcbd211dc54522d603c196f9f8fe3bb
fawazabdganiyu <fawazabdganiyu@gmail>
2024-01-05 08:17:43 +0100

    fix: review code

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-
 const wedgeAntillesUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+
 request(url, (err, response, body) => {
-  if (err) {
-    console.error(err);
-  } else {
-    try {
-      const filmsData = JSON.parse(body).results;
-      const filmsWithWedge = filmsData.filter((film) => {
-	return film.characters.includes(wedgeAntillesUrl);
-      });
-      console.log(filmsWithWedge.length)
-    } catch (err) {
-      console.error(err);
-    }
+  if (!err) {
+    const filmsData = JSON.parse(body).results;
+    const filmsWithWedge = filmsData.filter((film) => {
+      return film.characters.includes(wedgeAntillesUrl);
+    });
+    console.log(filmsWithWedge.length);
   }
 });
