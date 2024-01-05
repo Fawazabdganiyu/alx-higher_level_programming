c45ab2e8e2a963aee4f6a3112d499247544679e4
fawazabdganiyu <fawazabdganiyu@gmail>
2024-01-05 08:38:03 +0100

    fix: replace some method with includes

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-const wedgeAntillesUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
 
 request(url, (err, response, body) => {
   if (!err) {
     const filmsData = JSON.parse(body).results;
     const filmsWithWedge = filmsData.filter((film) => {
-      return film.characters.includes(wedgeAntillesUrl);
+      return film.characters.some((characterUrl) =>
+        characterUrl.endsWith('18/'));
     });
     console.log(filmsWithWedge.length);
   }
