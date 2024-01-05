3755c2c814ab3daad7929c29fdc4e785ba15044f
fawazabdganiyu <fawazabdganiyu@gmail>
2024-01-05 05:50:03 +0100

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+const wedgeAntillesUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    try {
+      const films = JSON.parse(body).results;
+      let count = 0;
+      for (const film of films) {
+	for (const character of film.characters) {
+	  if (character === wedgeAntillesUrl) {
+	    count++;
+	  }
+	}
+      }
+      console.log(count);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+});
