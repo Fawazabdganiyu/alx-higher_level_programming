93ce8c09e773561626e3b1a22a72d0d8dbfdcc02
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-01-12 11:37:28 +0100

    Get Star Wars movies title

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const url = 'https://swapi-api.alx-tools.com/api/films/?format=json';
+$.get(url, (data) => {
+  $.each(data.results, (index, movie) => {
+    $('UL#list_movies').append('<LI>' + movie.title + '</LI>');
+  });
+});
